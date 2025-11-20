8840a6cd21193ffffe3cc022183086d402ff989e
Cristiano Pessoa <cristianopessoa@gmail.com>
2025-11-20 20:19:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8411247969-7a5ee",
+  "appId": "1:588836587560:web:333b5d562e967f0cc40d1d",
+  "apiKey": "AIzaSyB1g36JpK7bTZlj5xtd1f9emjUYrIhI0ec",
+  "authDomain": "studio-8411247969-7a5ee.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "588836587560"
+};
